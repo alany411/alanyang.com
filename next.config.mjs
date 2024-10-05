507a04d6733f361ca0ffcc73cb5f7cfac83c4377
Alan Yang <me@alanyang.com>
2024-10-04 20:44:38 -0400

    feat: add syntax highlighting

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,7 @@ import createMDX from '@next/mdx'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ['mdx', 'ts', 'tsx'],
+  reactStrictMode: true,
   experimental: {
     mdxRs: true,
   },
