ce6d8e2cae7e8ecee1fc527ca13c208abecabb3f
Alan Yang <me@alanyang.com>
2024-10-07 18:18:26 -0400

    chore: update mdx-components

--- mdx-components.tsx
+++ mdx-components.tsx
@@ -1,33 +1,44 @@
 import type { MDXComponents } from 'mdx/types'
-import type { ComponentPropsWithoutRef } from 'react'
 import { Fragment } from 'react'
 import SyntaxHighlighter from 'react-syntax-highlighter'
 
-const Code = ({ children, className }: ComponentPropsWithoutRef<'code'>) => {
-  const language = className?.replace('language-', '')
-
-  if (!language) {
-    return (
-      <code className='bg-neutral-100 py-1 dark:bg-neutral-800'>
-        {children}
-      </code>
-    )
-  }
-
-  return (
-    <SyntaxHighlighter
-      className='border border-neutral-300 dark:border-neutral-700'
-      language={language}
-      useInlineStyles={false}
-    >
-      {/* SyntaxHighlighter throws a fit if we don't render the children as a string */}
-      {children as string}
-    </SyntaxHighlighter>
-  )
-}
-
 const customComponents: MDXComponents = {
-  code: Code,
+  a: (props) => {
+    const { children } = props
+
+    const ariaLabel =
+      props.href?.startsWith('/') || props.href?.startsWith('#')
+        ? undefined
+        : `${children as string}, opens in new tab`
+
+    return (
+      <a aria-label={ariaLabel} {...props}>
+        {children}
+      </a>
+    )
+  },
+  code: ({ children, className }) => {
+    const language = className?.replace('language-', '')
+
+    if (!language) {
+      return (
+        <code className='bg-neutral-100 py-1 dark:bg-neutral-800'>
+          {children}
+        </code>
+      )
+    }
+
+    return (
+      <SyntaxHighlighter
+        className='border border-neutral-300 dark:border-neutral-700'
+        language={language}
+        useInlineStyles={false}
+      >
+        {/* SyntaxHighlighter throws a fit if we don't render the children as a string */}
+        {children as string}
+      </SyntaxHighlighter>
+    )
+  },
   pre: Fragment,
 }
 
