e91f10a4e409d205bf3892b23d6a722d9b5e3f42
alany411 <alanyang411@gmail.com>
2024-10-20 02:55:37 -0400

    feat: add security headers

--- next.config.mjs
+++ next.config.mjs
@@ -11,6 +11,56 @@ const nextConfig = {
     mdxRs: false,
     reactCompiler: true,
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: `
+                    default-src 'self' vercel.live;
+                    script-src 'self' 'unsafe-eval' 'unsafe-inline' cdn.vercel-insights.com vercel.live va.vercel-scripts.com;
+                    style-src 'self' 'unsafe-inline';
+                    img-src * blob: data:;
+                    media-src 'none';
+                    connect-src *;
+                    font-src 'self' data:;
+                    frame-src 'self' vercel.live;
+                  `.replace(/\n/g, ''),
+          },
+          {
+            key: 'Permissions-Policy',
+            value: 'camera=(), microphone=(), geolocation=()',
+          },
+          {
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
+          },
+          {
+            key: 'Strict-Transport-Security',
+            value: 'max-age=31536000; includeSubDomains; preload',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+        ],
+      },
+    ]
+  },
   async redirects() {
     return [
       {
