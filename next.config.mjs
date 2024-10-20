4b4c57f6fcee3df0a7189e50f234cd0bccee326d
alany411 <alanyang411@gmail.com>
2024-10-20 04:28:04 -0400

    chore: update security headers

--- next.config.mjs
+++ next.config.mjs
@@ -20,7 +20,7 @@ const nextConfig = {
             key: 'Content-Security-Policy',
             value: `
               default-src 'self';
-              script-src 'self' https://cdn.vercel-insights.com https://va.vercel-scripts.com;
+              script-src 'self' https://cdn.vercel-insights.com https://va.vercel-scripts.com https://vercel.live 'sha256-OBTN3RiyCV4Bq7dFqZ5a2pAXjnCcCYeTJMO2I/LYKeo=' 'sha256-JQMxaFGDmeqTJsj5aZwkXDUj15Vi4w3cVdckV5APT0Q=' 'sha256-4gNMjCUgSqEbm/c1qsMc4ikMSrVyoMwGmKsBJ6vrNV0=' 'sha256-I58WLd8b6rWZqcm3Rs8ziPrevCZ7Q+kN208hUBa32CQ=' 'sha256-T2jo8MOEy/DgLn7vrieYAcHgXXI8IIs4znenPmr6A6Y=' 'sha256-s9uSRn7dRbXci0NwvDbS8ZCjtbN+I99eWAze17HVXME=' 'sha256-6Tn+5fOrALmOpmYCtqeBqhvUTNx9siW+NKMvd5MXdMc=';
               style-src 'self' 'unsafe-inline';
               img-src 'self';
               media-src 'none';
