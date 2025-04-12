8464e727c9d86064f942374d035b9bf4c562dbd5
Alan Yang <me@alanyang.com>
2025-04-12 14:08:54 -0400

    fix: margins

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -14,7 +14,7 @@ export default function NotFound() {
     <div>
       <Nav />
       <div className={cn('mb-6 flex')}>
-        <h1 className={cn('m-0')}>Not Found</h1>
+        <h1 className={cn('mt-0 mb-0')}>Not Found</h1>
       </div>
       <p className={cn('text-xl font-medium text-[var(--tw-prose-headings)]')}>
         You seem to be lost...
