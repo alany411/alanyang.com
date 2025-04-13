472daad463d3066ad98ba9846c11e2d56fb76450
Alan Yang <me@alanyang.com>
2025-04-13 15:55:27 -0400

    chore: remove cssnano

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,9 +1,5 @@
-/** @type {import('postcss-load-config').Config} */
 const config = {
-  plugins: {
-    '@tailwindcss/postcss': {},
-    ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {}),
-  },
+  plugins: ['@tailwindcss/postcss'],
 }
 
 export default config
