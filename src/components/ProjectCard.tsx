9b0683a41054ad7aaf4d7d944d43709f982005df
Alan Yang <me@alanyang.com>
2025-04-13 02:55:43 -0400

    fix: ProjectCard link

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -13,24 +13,24 @@ export default function ProjectCard({ title, description, slug }: Project) {
     >
       <h2 className={cn('mt-0 mb-0')}>{title}</h2>
       <p className={cn('mt-0 mb-4')}>{description}</p>
-      <Link
-        aria-label={`View details for ${title}`}
-        href={`/projects/${slug}`}
-        className={cn(
-          'mt-0 mb-0 inline-flex items-center justify-center gap-1'
-        )}
-      >
-        View Details
+      <div className={cn('inline-block')}>
+        <Link
+          aria-label={`View details for ${title}`}
+          className='inline align-baseline'
+          href={`/projects/${slug}`}
+        >
+          View Details
+        </Link>
         <MoveRightIcon
           aria-hidden={true}
           className={cn(`
-            size-4 transition-all
+            ml-1 inline size-4 align-middle transition-all
             group-hover:translate-x-0.5 group-hover:text-sky-500
             group-focus:text-sky-500
             dark:group-hover:text-sky-400 dark:group-focus:text-sky-400
           `)}
         />
-      </Link>
+      </div>
     </div>
   )
 }
