f645df79aee3d90b8b0f2de8e957327d7c611f0b
alany411 <alanyang411@gmail.com>
2025-04-20 13:51:39 -0400

    chore: remove some headers

--- next.config.mjs
+++ next.config.mjs
@@ -96,18 +96,6 @@ const nextConfig = {
             key: 'X-XSS-Protection',
             value: '1; mode=block',
           },
-          {
-            key: 'Cross-Origin-Embedder-Policy',
-            value: 'require-corp',
-          },
-          {
-            key: 'Cross-Origin-Opener-Policy',
-            value: 'same-origin',
-          },
-          {
-            key: 'Cross-Origin-Resource-Policy',
-            value: 'same-origin',
-          },
         ],
       },
     ]
