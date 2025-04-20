2034e9e27016b651b61bbe028e864ee0a3931dda
alany411 <alanyang411@gmail.com>
2025-04-20 12:43:48 -0400

    feat: increase PhotoGrid size

--- src/app/photos/page.tsx
+++ src/app/photos/page.tsx
@@ -18,7 +18,12 @@ export default async function Photos() {
       <div className={cn('mb-6 flex')}>
         <h1 className={cn('mt-0 mb-0')}>Photos</h1>
       </div>
-      <div className={cn('mt-10')}>
+      <div
+        className={cn(`
+          mx-0 mt-10
+          md:-mx-20
+        `)}
+      >
         <PhotoGrid photos={photos} />
       </div>
     </div>
